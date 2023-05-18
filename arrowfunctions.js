65b1871a742f45e2a1bb03da06d5e340ac8eb7b0
edu290386 <eduardo.torres@outlook.com>
2023-05-17 23:27:01 -0500

    code v2.0

--- arrowfunctions.js
+++ arrowfunctions.js
@@ -4,3 +4,23 @@ const sumar = (a,b,...c) => {
     return resultado
 }
 console.log(sumar(1,2,3,4,5,6,7))
+
+const perro = {
+    nombre: "Dustin",
+    raza: "Schanuzer",
+    ladrar() {
+        console.log("Guau");
+        console.log(this.raza);
+    }
+}
+perro.ladrar()
+
+function sumar2(a,b,...c){
+    let resultado = a+b;
+    c.forEach(function(element){
+        resultado += element
+        console.log(resultado);
+    })
+    return resultado
+}
+console.log(sumar2(1,2,3,4,5,6,7))
