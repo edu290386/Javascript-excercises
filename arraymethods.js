90139d1dcfa104e3fc44b2fb4e6590d1cbd38bca
edu290386 <eduardo.torres@outlook.com>
2023-05-18 15:19:26 -0500

    array methods v2.2

--- arraymethods.js
+++ arraymethods.js
@@ -27,4 +27,32 @@ const countVowels = arrOfVowels.reduce((acc, val) => {
 console.log(countVowels);
 
 
+let object1 = {
+  firstName: "Nishant",
+  age: 24, 
+  salary: 300,
+}
+
+let object2 = {
+  lastName: "Kumar",
+  height: '20 meters',
+  weight: '70 KG'
+}
+
+let object3 = {...object1, ...object2}
+console.log(object3);
+console.log(object3.firstName, " ", object3.lastName)
+
+
+const users = [
+  { id: 11, name: 'Adam', age: 23, group: 'editor' },
+  { id: 47, name: 'John', age: 28, group: 'admin' },
+  { id: 85, name: 'William', age: 34, group: 'editor' },
+  { id: 97, name: 'Oliver', age: 28, group: 'admin' }
+];
+const updatedUsers = users.map( p => p.id !== 47 ? p : {...p, age: p.age + 5 });
+console.log(updatedUsers);
+console.log(users);
+const newData = users.map(({id, age, group}) => `\n${id} ${age} ${group}`).join('')
+console.log(newData);
 
